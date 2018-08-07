af8e6e952e49f27df423a9049e2062dbd216bb2f
Moquan Chen <mq.chen@gmail.com>
2018-08-07 16:30:18 +0200

    Updated example

--- example/app.js
+++ example/app.js
@@ -1,9 +1,26 @@
 const TaskHub = require('..');
 
-const hub = new TaskHub.Server({ port: 8080 });
-hub.addCredential('debug', {
-  key: 'password123lol',
+const config = {
+  port: 8080,
+  service: 'test',
+  key: 'password123lol'
+};
+
+const hub = new TaskHub.Server({ port: config.port });
+hub.addCredential(config.service, {
+  key: config.key,
   // subPermissions: ['mail:send-bulk'],
   // pubPermissions: ['log:error', 'log:warn', 'log:info', 'googlemaps'],
 });
+
+(async () => {
+  const client = await TaskHub.Client.create(`ws://localhost:${config.port}`);
+  client.sub('test:echo', async (task) => {
+    console.log(await task.getPayload());
+  });
+})();
+
+console.log(`RUN: wscat -c localhost:${config.port} -H service:${config.service} -H key:${config.key}`);
+console.log('Available action: test:echo');
+
 hub.start();
